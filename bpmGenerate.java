d011f4a40ce7667d766a3f70bb0db2510f1b1c12
tmart61 <tjmartin61@gmail.com>
2020-12-01 11:13:52 -0500

    Update bpmGenerate.java

--- bpmGenerate.java
+++ bpmGenerate.java
@@ -1,3 +1,6 @@
+//**For testing and checking the math, use the "Quarter" column in the chart link in Line 2
+//https://guitargearfinder.com/wp-content/uploads/2014/03/bpm-to-ms.jpg
+
 import java.util.Math;
 
 public class bpmGenerate extends BPM_Generator_Driver {
@@ -7,16 +10,18 @@ public class bpmGenerate extends BPM_Generator_Driver {
 	private long tempoDiff;
 	
 	public long bpmGenerate {
-		
-	//We don't need to convert to milliseconds here because the capture in Chronograph does this for us
 	
+	//Calculate temporal distance between start and stop in milliseconds
 	tempoDiff = tempoEvent2-tempoEvent1;
 	
+	//Absolute value the tempoDiff to remove potenial negative from long
 	Math.abs(tempoDiff);
 	
-	tempoDiff = (tempoDiff/1000) * 60;
+	//Divide 60000 (number of milliseconds in a minute) by tempoDiff (tempo difference)
+	tempoDiff = (60000/tempoDiff);
 	
+	//Return BPM (beats per minute)
 	return tempoDiff;
-	
+		
 	} //end of class
 } // end of main
