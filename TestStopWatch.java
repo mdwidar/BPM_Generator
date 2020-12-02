6ef53a6a6034783a737528890b9315fbf4a67e62
mdwidar <70721405+mdwidar@users.noreply.github.com>
2020-12-02 16:57:55 -0500

    Update TestStopWatch.java

--- TestStopWatch.java
+++ TestStopWatch.java
@@ -1,8 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class TestStopWatch {
+import java.awt.Toolkit;
 
+public class TestStopWatch {
+	
+	
+	
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setLayout(new BorderLayout());
@@ -14,9 +18,14 @@ public class TestStopWatch {
         watch.setOpaque(true);
         watch.setSize(480, 480);
         frame.add(watch, BorderLayout.CENTER);
-
+                        
+        
         frame.setSize(new Dimension(480, 480));
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);    
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
+
+	
 }
