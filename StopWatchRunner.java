265c1c9aa6f6adfd0eb47b965992d7d2b4d8b88a
tmart61 <tjmartin61@gmail.com>
2020-12-03 14:42:27 -0500

    Update StopWatchRunner.java
    Reformatted and added comments/credits

--- StopWatchRunner.java
+++ StopWatchRunner.java
@@ -1,3 +1,10 @@
+/**CIS111BONLN
+* @author Tyler Martin, Marwa Dwidar, Charlie Bivinghouse
+* @date 12/17/2020
+* Final Project - BPM Generator
+* Original Creator/Design/Writer of StopWatchRunner - David J Eck.
+*/
+
 /*
    A component that acts as a simple stop-watch.  When the user clicks
    on it, this component starts timing.  When the user clicks again,
@@ -9,72 +16,71 @@
 import java.awt.event.*;
 import javax.swing.*;
 
-public class StopWatchRunner extends JLabel
-            implements MouseListener, ActionListener {
+public class StopWatchRunner extends JLabel implements MouseListener, ActionListener {
 
-   /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L; //Required by Eclipse for versioning.
 
-private long startTime;   // Start time of stopwatch.
-                             //   (Time is measured in milliseconds.)
+   private long startTime;   // Start time of stopwatch, measured in milliseconds.
 
    private boolean running;  // True when the stopwatch is running.
 
-   private Timer timer;  // A timer that will generate events
-                         // while the stopwatch is running
+   private Timer timer;  // A timer that will generate events while the stopwatch is running
 
    private double beatsPerMinute; //Calculates the beats per minute
 
-   public StopWatchRunner() {
-         // Constructor.
+   // Constructor
+   public StopWatchRunner() { 
       super("  Click to start timer.  ", JLabel.CENTER);
       addMouseListener(this);
    }
-
+   
+   // This will be called when an event from the
+   // timer is received.  It just sets the stopwatch
+   // to show the amount of time that it has been running.
+   // Time is rounded down to the nearest second.
+   
    public void actionPerformed(ActionEvent evt) {
-          // This will be called when an event from the
-          // timer is received.  It just sets the stopwatch
-          // to show the amount of time that it has been running.
-          // Time is rounded down to the nearest second.
        long time = (System.currentTimeMillis() - startTime);
        setText("Running:  " + time + " milliseconds");
        
    }
 
+   // React when user presses the mouse by
+   // starting or stopping the stopwatch.  Also start
+   // or stop the timer.
+   
    public void mousePressed(MouseEvent evt) {
-          // React when user presses the mouse by
-          // starting or stopping the stopwatch.  Also start
-          // or stop the timer.
       if (running == false) {
-            // Record the time and start the stopwatch.
+         
+    	 // Record the time and start the stopwatch.
          running = true;
          startTime = evt.getWhen();  // Time when mouse was clicked.
          setText("Running:  0 milliseconds");
-         if (timer == null) {
-            timer = new Timer(10,this);
-            timer.start();
-         }
-         else
-            timer.restart();
+	         if (timer == null) {
+	            timer = new Timer(10,this);
+	            timer.start();
+	         }
+	         else
+	            timer.restart();
       }
       else {
-            // Stop the stopwatch.  Compute the elapsed time since the
-            // stopwatch was started and display it.
+         // Stop the stopwatch.  Compute the elapsed time since the
+         // stopwatch was started and display it.
          timer.stop();
          running = false;
          long endTime = evt.getWhen();
          double milliseconds = (endTime - startTime);
          
-         	//Added code to display the beats per minute
+         //Calculate BPM (beats per minute) ratio from milliseconds
          beatsPerMinute = (60000/milliseconds);
         
+         //Display stopwatch milliseconds and BPM calculation result
          setText("<html> BPM Generator" + "<br>" + "Distance between inputs: " + milliseconds + " milliseconds." + "<br>" + "The tempo is: " + Math.round(beatsPerMinute) + " beats per minute.<html>");
          
       }
    }
-
+   
+   //JavaEvent Listeners
    public void mouseReleased(MouseEvent evt) { }
    public void mouseClicked(MouseEvent evt) { }
    public void mouseEntered(MouseEvent evt) { }
