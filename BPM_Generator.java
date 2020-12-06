2014a920a0a1b6046251ad08c68ad9ef13052c2a
tmart61 <tjmartin61@gmail.com>
2020-12-06 13:59:18 -0500

    Update BPM_Generator.java

--- BPM_Generator.java
+++ BPM_Generator.java
@@ -1,12 +1,12 @@
 /**CIS111BONLN
 * @author Tyler Martin, Marwa Dwidar, Charlie Bivinghouse
 * @date 12/17/2020
-* Final Project - BPM_Generator
-* Original Creator/Design/Writer of StopWatchRunner(Changed name to fit our purpose to BPM_Generator) - David J Eck.
+* Final Project - BPM Generator
+* Original Creator/Design/Writer of StopWatchRunner - David J Eck.
 */
 
 /*
-   A component that acts as a simple BPM_Generator.  When the user clicks
+   A component that acts as a simple stop-watch.  When the user clicks
    on it, this component starts timing.  When the user clicks again,
    it displays the time between the two clicks.  Clicking a third time
    starts another timer, etc.  While it is timing, the label just
@@ -20,13 +20,13 @@ public class BPM_Generator extends JLabel implements MouseListener, ActionListen
 
    private static final long serialVersionUID = 1L; //Required by Eclipse for versioning.
 
-   private long startTime;   // Start time of BPM Generator, measured in milliseconds.
+   private long startTime;   // Start time of stopwatch, measured in milliseconds.
 
-   private boolean running;  // True when the BPM Generator is running.
+   private boolean running;  // True when the stopwatch is running.
 
-   private Timer timer;  // A timer that will generate events while the BPM Generator is running
+   private Timer timer;  // A timer that will generate events while the stopwatch is running
 
-   private double beatsPerMinute; //Calculates the beats per minute
+   public double beatsPerMinute; //Calculates the beats per minute
 
    // Constructor
    public BPM_Generator() { 
@@ -35,7 +35,7 @@ public class BPM_Generator extends JLabel implements MouseListener, ActionListen
    }
    
    // This will be called when an event from the
-   // timer is received.  It just sets the BPM Generator
+   // timer is received.  It just sets the stopwatch
    // to show the amount of time that it has been running.
    // Time is rounded down to the nearest second.
    
@@ -44,7 +44,7 @@ public class BPM_Generator extends JLabel implements MouseListener, ActionListen
        setText("Running:  " + time + " milliseconds");
        
    }
-
+   
    // React when user presses the mouse by
    // starting or stopping the stopwatch.  Also start
    // or stop the timer.
@@ -52,7 +52,7 @@ public class BPM_Generator extends JLabel implements MouseListener, ActionListen
    public void mousePressed(MouseEvent evt) {
       if (running == false) {
          
-    	 // Record the time and start the BPM Generator.
+    	 // Record the time and start the stopwatch.
          running = true;
          startTime = evt.getWhen();  // Time when mouse was clicked.
          setText("Running:  0 milliseconds");
@@ -64,8 +64,8 @@ public class BPM_Generator extends JLabel implements MouseListener, ActionListen
 	            timer.restart();
       }
       else {
-         // Stop the BPM Generator.  Compute the elapsed time since the
-         // BPM Generator was started and display it.
+         // Stop the stopwatch.  Compute the elapsed time since the
+         // stopwatch was started and display it.
          timer.stop();
          running = false;
          long endTime = evt.getWhen();
@@ -77,6 +77,10 @@ public class BPM_Generator extends JLabel implements MouseListener, ActionListen
          //Display stopwatch milliseconds and BPM calculation result
          setText("<html> BPM Generator" + "<br>" + "Distance between inputs: " + milliseconds + " milliseconds." + "<br>" + "The tempo is: " + Math.round(beatsPerMinute) + " beats per minute.<html>");
          
+         //Play metronome of BPM
+         BPM_Generator_Metronome_beep metronome1 = new BPM_Generator_Metronome_beep(beatsPerMinute,1);
+      	 metronome1.start();
+         
       }
    }
    
@@ -86,4 +90,4 @@ public class BPM_Generator extends JLabel implements MouseListener, ActionListen
    public void mouseEntered(MouseEvent evt) { }
    public void mouseExited(MouseEvent evt) { }
 
-}  // end BPM_Generator
+}  // end StopWatchRunner
